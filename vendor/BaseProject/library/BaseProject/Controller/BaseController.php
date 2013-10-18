21fd79ed96615895e3156f6b63a0ceb4a38c5162
musicogui <musicogui@gmail.com>
2013-10-17 20:45:18 -0500

    BaseProject + composer

---
+++ vendor/BaseProject/library/BaseProject/Controller/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * @author isdarka
+ * @created Aug 25, 2013 11:03:24 PM
+ */
+
+namespace BaseProject\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class BaseController extends AbstractActionController
+{
+	public function __construct(){
+	}
+}
---
+++ vendor/BaseProject/library/BaseProject/Model/Collection/Interfaces/InterfaceCollection.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ *
+ * @author isdarka
+ * @created Aug 26, 2013 6:32:00 PM
+ */
+namespace BaseProject\Model\Collection\Interfaces;
+
+interface InterfaceCollection
+{
+	/**
+	 * Appends the value
+	 * @param Collectable $collectable
+	 */
+	public function append($collectable);
+	
+	/**
+	 * Return current array entry
+	 * @return Collectable
+	 */
+	public function current();
+	
+	/**
+	 * Return current array entry and
+	 * move to next entry
+	 * @return Collectable
+	 */
+	public function read();
+	
+	/**
+	 * Get the first array entry
+	 * if exists or null if not
+	 * @return Collectable|null
+	 */
+	public function getOne();
+	
+	/**
+	 * Remove one collectable with $name
+	 * @param  int $index
+	 */
+	public function remove($index);
+	
+	/**
+	 * Retrieve the array with primary keys
+	 * @return array
+	 */
+	public function getPrimaryKeys();
+	
+	/**
+	 * Retrieve the Collectable with primary key
+	 * @param  int $name
+	 * @return Collectable
+	 */
+	public function getByPK($index);
+	
+	/**
+	 * Is Empty
+	 * @return boolean
+	 */
+	public function isEmpty();
+	
+	/**
+	 * convert to array
+	 * @return array
+	 */
+	public function toArray();
+	
+	/**
+	 * convert to json
+	 * @return string
+	 */
+	public function toJson();
+	
+}
